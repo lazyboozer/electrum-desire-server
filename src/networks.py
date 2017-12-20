b7c057b85fd646d26eab994af2306641ed131551
Yan Hülsebusch <yh@dezign.de>
2017-12-20 12:22:30 +0100

    no message

--- src/networks.py
+++ src/networks.py
@@ -2,12 +2,12 @@
 
 # Dash src/chainparams.cpp
 params = {
-    'dash_main': {
+    'desire_main': {
         'pubkey_address': 30, #L120
         'script_address': 16, #L122
         'genesis_hash': '0x00000f79a81b6318e0f36dc486adf4bb5bb1fa34025d69b991893c42978c2027' #L110
     },
-    'dash_test': {
+    'desire_test': {
         'pubkey_address': 140, #L220
         'script_address': 19, #L222
         'genesis_hash': '0x00000f79a81b6318e0f36dc486adf4bb5bb1fa34025d69b991893c42978c2027' #L210
