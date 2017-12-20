9d03c0e25ae561453944df78f43088d5bca85e85
Yan Hülsebusch <yh@dezign.de>
2017-12-20 12:21:01 +0100

    desire support added

--- src/networks.py
+++ src/networks.py
@@ -3,13 +3,13 @@
 # Dash src/chainparams.cpp
 params = {
     'dash_main': {
-        'pubkey_address': 76, #L120
+        'pubkey_address': 30, #L120
         'script_address': 16, #L122
-        'genesis_hash': '00000ffd590b1485b3caadc19b22e6379c733355108f107a430458cdf3407ab6' #L110
+        'genesis_hash': '0x00000f79a81b6318e0f36dc486adf4bb5bb1fa34025d69b991893c42978c2027' #L110
     },
     'dash_test': {
         'pubkey_address': 140, #L220
         'script_address': 19, #L222
-        'genesis_hash': '00000bafbc94add76cb75e2ec92894837288a481e5c005f6563d91623bf8bc2c' #L210
+        'genesis_hash': '0x00000f79a81b6318e0f36dc486adf4bb5bb1fa34025d69b991893c42978c2027' #L210
     }
 }
